1667b4ffdc1c982e74a3d57f5f5d50c4d7f50d61
FOUAAHA <fouad.ahaaha@educa.jcyl.es>
2024-11-07 18:50:13 +0100

    chore: ultimas puntadas antes de poner el proyecto a punto

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.tarea05ddi"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.tarea05ddi"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
