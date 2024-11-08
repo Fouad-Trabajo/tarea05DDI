faf4899feb15ebf460e69ed215cde759b60bb084
Fouad (Fuaa) Aharchi Aharchi <fouad.ahaaha@educa.jcyl.es>
2024-11-08 15:00:58 +0100

    feat: funcionalidad funcionando, ahora solo falta los estilos y temas de la view

--- app/src/main/java/com/example/tarea05ddi/MainActivity.kt
+++ app/src/main/java/com/example/tarea05ddi/MainActivity.kt
@@ -1,20 +1,35 @@
 package com.example.tarea05ddi
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.tarea05ddi.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        buttons()
+    }
+
+    private fun buttons() {
+        /* Convertimos el texto escrito en el editText, por el usuario
+             en un string y se lo asignamos a los textView */
+        binding.button1.setOnClickListener {
+            val textReceive = binding.editText.text.toString()
+            binding.text1.text = textReceive
+            binding.text2.text = textReceive
+        }
+
+        binding.button2.setOnClickListener {
+            Toast.makeText(this, "Se ha pulsado el botón 2", Toast.LENGTH_SHORT).show()
         }
     }
 }
